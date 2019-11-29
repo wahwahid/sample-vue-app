af0849170b341cba6724b1eaaec45dcc2ecc0591
wahwahid <wahid.dulrohman@gmail.com>
2019-11-29 22:14:38 +0700

    Work with Form

--- src/ContactApp.vue
+++ src/ContactApp.vue
@@ -1,43 +1,51 @@
 <template>
   <div id="app" class="small-container">
     <h1>Contacts</h1>
+    <contact-form @add:contact="addContact" />
     <contact-list :contacts="contacts" />
   </div>
 </template>
 <script>
-import ContactList from '@/components/ContactList.vue'
+import ContactList from "@/components/ContactList.vue";
+import ContactForm from "@/components/ContactForm.vue";
 
 export default {
-  name: 'contact-app',
+  name: "contact-app",
   components: {
     ContactList,
+    ContactForm
   },
   data() {
     return {
       contacts: [
         {
           id: 1,
-          name: 'Jon Snow',
-          email: 'jon@snow.north',
+          name: "Jon Snow",
+          email: "jon@snow.north"
         },
         {
           id: 2,
-          name: 'Arya Stark',
-          email: 'arya@stark.north',
+          name: "Arya Stark",
+          email: "arya@stark.north"
         },
         {
           id: 3,
-          name: 'Daenerys Targaryen',
-          email: 'danny@targaryen.dragon',
-        },
-      ],
-    }
+          name: "Daenerys Targaryen",
+          email: "danny@targaryen.dragon"
+        }
+      ]
+    };
   },
-}
+  methods: {
+    addContact(contact) {
+      this.contacts = [...this.contacts, contact];
+    }
+  }
+};
 </script>
 
 <style>
-  .small-container {
-    max-width: 680px;
-  }
+.small-container {
+  max-width: 680px;
+}
 </style>
---
+++ src/components/ContactForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="contact-form">
+    <form @submit.prevent="handleSubmit">
+      <label>Contact name</label>
+      <input v-model="contact.name" type="text" />
+      <label>Contact Email</label>
+      <input v-model="contact.email" type="text" />
+      <button>Add Contact</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "contact-form",
+  data() {
+    return {
+      contact: {
+        name: "",
+        email: ""
+      }
+    };
+  },
+  methods: {
+    handleSubmit() {
+      const id = Date.now()
+      this.$emit("add:contact", { id, ...this.contact });
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin-bottom: 40px;
+}
+</style>
